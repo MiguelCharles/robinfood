0cfe84790d14256efad6fa1ce6e47dc1f164441d
Martin Bragard <bragard.martin@gmail.com>
2016-12-07 12:35:15 +0100

    non important change in the order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   def index
-    @orders = Order.all.order(:updated_at)
+    @orders = Order.all.order(:updated_at) #maybe better to order by status
   end
 
   def show
@@ -12,15 +12,26 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new()
+
+  # order = Order.find(params[:order_id])
+  #   order.buyer_id = params[:user_id]
+  #   order.status = "To be confirmed"
+  #   order.save!
+  #   promotion = Promotion.find(params[:promotion_id])
+  #   @orderline = OrderLine.create(order_id: order.id, promotion_id: promotion.id, status: "To be confirmed")
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
   end
 
 private
