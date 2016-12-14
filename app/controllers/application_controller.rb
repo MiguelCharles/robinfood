0ab77ce8dc4ea543d145c813ebcf3b2f1b60cdbd
Martin Bragard <bragard.martin@gmail.com>
2016-12-14 15:19:00 +0100

    orders without login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,9 @@ private
   # and signing up work automatically.
   def store_current_location
     store_location_for(:user, request.url)
+
+  end
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :destroy, :add_to_order]
-#  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:update]
   def index
-
+    unless session[:order].nil?
+      @order = Order.new(session[:order])
+      @order.user = current_user
+      @order.save!
+      session[:order] = nil
+    end
     @orders = current_user.orders.order(:updated_at) #maybe better to order by status
   end
 
@@ -20,15 +25,14 @@ class OrdersController < ApplicationController
 
   def create
     if current_user
+
       @order = Order.new(order_params)
        @order.user_id = current_user.id
       @order.status = "To be confirmed"
       @order.save!
-           redirect_to orders_path
-
+      redirect_to orders_path
     else
-      @order = Order.new(order_params)
-      @order.save!
+      session[:order] = order_params
       redirect_to new_user_session_path
     end
   end
@@ -39,16 +43,10 @@ class OrdersController < ApplicationController
   end
 
   def update
-    if current_user
       @order = Order.find(params[:id])
       @order.update!(order_params)
            redirect_to orders_path
 
-    else
-       @order = Order.new(order_params)
-      @order.save!
-      redirect_to new_user_session_path
-    end
   end
 
   def destroy
