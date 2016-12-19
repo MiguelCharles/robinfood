8bb9f1a7a25ab44fd629a23ceb3331f9cd30457e
Martin Bragard <bragard.martin@gmail.com>
2016-12-19 11:59:45 +0100

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,47 +16,43 @@ User.create(username:"François", email:"francois@gmail.com", password:"123456" 
 User.create(username:"Martin",email:"martin@gmail.com", password:"123456", address: "Liège", facebook_picture_url:"user/MartinB.jpg")
 User.create(username:"Dandoy",email:"admin@dandoy.com", password:"123456" , address: "Anderlecht", facebook_picture_url:"MaisonDandoy.jpg")
 User.create(username:"Marcel",email:"admin@marcel.com", password:"123456" , address: "Verviers", facebook_picture_url:"Marcel.jpg")
+User.create(username:"Sushi Shop",email:"admin@sushishop.com", password:"123456" , address: "Bruxelles", facebook_picture_url:"sushishop.jpg")
+User.create(username:"Eat & Drink",email:"admin@eatanddrink.com", password:"123456" , address: "Bruxelles", facebook_picture_url:"eatanddrink.jpg")
 
 Shop.create(user_id: User.all.first.id, name_of_the_store: "Exki", photo_url: "exki.jpg", category:"Grand Place", description: "Cuisiner des produits de saison, valoriser les végétaux, donner la préférence aux agriculteurs respectueux de la planès", phone_number: "+32 479 22 55 66", VAT_number: "ABCD", country:"Belgium", city:"Brussels", zip_code: 1000, address:"
 Rue du Marché aux Herbes", street_number: 93)
 id = (User.all.last.id) - 1
 Shop.create(user_id: id, name_of_the_store: "Maison Dandoy", photo_url: "Dandoy.jpg", category:"Biscuiterie", description:"We want to manke your sense dance", phone_number:"+32 56 33 23 47" ,VAT_number: "AZERTY", country:"Belgium", city:"Bruxelles", zip_code: 1000, address:"Rue au Beurre", street_number: 31)
-Shop.create(user_id: User.all.last.id, name_of_the_store: "Chez Marcel", photo_url: "Marcel.jpg", category:"Fine Grocery", description: "Votre boulanger de tradition qui aime le gout", phone_number:"+32 56 33 23 47" ,VAT_number: "AZERTY", country:"Belgium", city:"Ixelles", zip_code: 1050, address:"Avenue Louise", street_number: 200)
+Shop.create(user_id: id, name_of_the_store: "Chez Marcel", photo_url: "Marcel.jpg", category:"Grocery", description: "Votre boulanger de tradition qui aime le gout", phone_number:"+32 56 33 23 47" ,VAT_number: "AZERTY", country:"Belgium", city:"Ixelles", zip_code: 1050, address:"Avenue Louise", street_number: 200)
+Shop.create(user_id: id, name_of_the_store: "Sushi Shop", photo_url: "sushishop.jpg.jpg", category:"Sushis", description: "Restaurant japonais", phone_number:"+32 56 33 23 47" ,VAT_number: "AZERTY", country:"Belgium", city:"Bruxelles", zip_code: 1000, address:"Place du Grand Sablon", street_number: 10)
+Shop.create(user_id: User.all.last.id, name_of_the_store: "Eat & Drink", photo_url: "eatanddrink.jpg.jpg", category:"Sandwich & soup", description: "Best place for a hot lunch", phone_number:"+32 56 33 23 47" ,VAT_number: "AZERTY", country:"Belgium", city:"Bruxelles", zip_code: 1000, address:"Rue des Colonies", street_number: 50)
 
-ingredients = ["Fruit", "Vegetable", "Bread", "Christmas"]
+ingredients = ["Fruit", "Vegetable", "Bread", "Christmas", "Sushis", "Pasta", "Soup"]
 
-fruits_t = ["Jonnagold Apple", "Orange from Valencia"]
-fruits_tpic = ["food/fruits/apple.jpg","food/fruits/orange.jpg"]
-
-
-vegetables_t = ["Cabbages", "Celery", "Zuchini"]
-vegetables_tpic = ["food/vegetables/potatoes.jpg","food/vegetables/celery.jpg", "food/vegetables/zuchini.jpg"]
-
-breads_t = ["Cougnou", "Cramique"]
-breads_tpic = ["food/bread/cougnou.jpg","food/bread/complete.jpg"]
-
-sandwich_t = ["John", "Luca", "Louisa"]
-sandwich_tpic = ["food/sandwich/cornichon.jpg","food/sandwich/roasted.jpg","food/sandwich/tomato.jpg" ]
-
-christmas_t = ["chutney", "chocolate cake"]
-christmas_tpic = ["food/christmas/confiture.jpg","food/christmas/chocolate.jpg"]
-
-fruits_f = [ "Belgian Pear Doyen", "Strawberry from Wepion"]
-fruits_fpic = ["food/fruits/night.jpg","food/fruits/strawberry.jpg"]
+fruits_t = ["Jonnagold Apple", "Orange from Valencia", "Belgian Pear Doyen", "Strawberry from Wepion"]
+fruits_tpic = ["food/fruits/apple.jpg","food/fruits/orange.jpg", "food/fruits/night.jpg","food/fruits/strawberry.jpg"]
 
 
-vegetables_f = ["Spinach", "Sweet Potato"]
-vegetables_fpic = ["food/vegetables/salad.jpg","food/vegetables/sweet-potatoe.jpg"]
+vegetables_t = ["Cabbages", "Celery", "Zuchini", "Spinach", "Sweet Potato"]
+vegetables_tpic = ["food/vegetables/potatoes.jpg","food/vegetables/celery.jpg", "food/vegetables/zuchini.jpg", "food/vegetables/salad.jpg","food/vegetables/sweet-potatoe.jpg"]
 
-breads_f = ["Croissant", "Baguette"]
-breads_fpic = ["food/bread/croissant.jpg","food/bread/baguette.jpg"]
+breads_t = ["Cougnou", "Cramique", "Croissant", "Baguette"]
+breads_tpic = ["food/bread/cougnou.jpg","food/bread/complete.jpg","food/bread/croissant.jpg","food/bread/baguette.jpg"]
 
-sandwich_f = ["Big Poulet", "Marcos", "DeliEggs"]
-sandwich_fpic = ["food/sandwich/salami.jpg","food/sandwich/tomato.jpg", "food/sandwich/salami.jpg"]
+sandwich_t = ["John", "Luca", "Louisa","Big Poulet", "Brocoli soup", "DeliEggs"]
+sandwich_tpic = ["food/sandwich/cornichon.jpg","food/sandwich/roasted.jpg","food/sandwich/tomato.jpg","food/sandwich/salami.jpg","sandwich.jpg", "food/sandwich/salami.jpg" ]
 
-christmas_f = ["cupcake", "gingerbread"]
-christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
+christmas_t = ["chutney", "chocolate cake","cupcake", "gingerbread"]
+christmas_tpic = ["food/christmas/confiture.jpg","food/christmas/chocolate.jpg", "food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
 
+sushi_t = ["Lunch Box", "Sushi Box"]
+sushi_tpic = ["sushis1.jpg","sushis2.jpg"]
+
+pasta_t = ["Carbonara", "Bolognese"]
+pasta_tpic = ["carbo.jpg","bolo3.jpg"]
+
+soup_t = ["Brocoli soup", "Fish soup"]
+soup_tpic = ["soupe_verte.jpg","soupe_orange.jpg"]
 
 
  shop_id = Shop.all.first.id
@@ -68,7 +64,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (4..6).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
  p discount
  discount2 = 1 - discount[rand(0..6)]
@@ -84,7 +80,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  unit: "Piece(s)",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
+ validity: Time.now + (3600..36000).to_a.sample,
  promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
@@ -93,7 +89,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  end
 
 
- shop_id = Shop.all.last.id
+ shop_id = Shop.all.first.id + 2
  shop = Shop.find(shop_id)
  product_type = "Fruit"
  for index in 0 ... fruits_t.size
@@ -102,7 +98,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (1..3).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
  discount2 = 1 - discount[rand(0..6)]
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
@@ -114,7 +110,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  unit: "kg",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
+ validity: Time.now + (3600..36000).to_a.sample,
  promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
@@ -123,7 +119,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  end
 
 
- shop_id = Shop.all.last.id
+ shop_id = Shop.all.first.id + 2
  shop = Shop.find(shop_id)
  product_type = "Vegetable"
  for index in 0 ... vegetables_t.size
@@ -132,7 +128,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (2..5).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
  discount2 = 1 - discount[rand(0..6)]
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
@@ -144,7 +140,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  unit: "kg",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
+ validity: Time.now + (3600..36000).to_a.sample,
  promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
@@ -152,7 +148,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  price_after_promotion_per_unit:price_after_promotion_per_unit)
  end
 
- shop_id = Shop.all.last.id
+ shop_id = Shop.all.first.id + 1
  shop = Shop.find(shop_id)
  product_type = "Bread"
  for index in 0 ... breads_t.size
@@ -161,7 +157,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (2..6).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
  discount2 = 1 - discount[rand(0..6)]
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
@@ -173,7 +169,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  unit: "piece",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
+ validity: Time.now + (3600..36000).to_a.sample,
  promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
@@ -182,7 +178,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  end
 
 
- shop_id = Shop.all.last.id - 1
+ shop_id = Shop.all.first.id + 2
  shop = Shop.find(shop_id)
  product_type = "Christmas"
  for index in 0 ... christmas_t.size
@@ -191,7 +187,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (3..7).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
  discount2 = 1 - discount[rand(0..6)]
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
@@ -203,7 +199,7 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  unit: "piece",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
+ validity: Time.now + (3600..36000).to_a.sample,
  promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
@@ -211,31 +207,33 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
  price_after_promotion_per_unit:price_after_promotion_per_unit)
  end
 
-# FALSE
-
- shop_id = Shop.all.first.id
+shop_id = Shop.all.last.id - 1
  shop = Shop.find(shop_id)
- product_type = "Sandwich"
- for index in 0 ... sandwich_t.size
-    title = sandwich_f[index]
-    photo_url = sandwich_fpic[index].to_s
+ product_type = "Sushis"
+ for index in 0 ... sushi_t.size
+    title = sushi_t[index]
+    photo_url = sushi_tpic[index].to_s
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (10..15).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+ p discount
  discount2 = 1 - discount[rand(0..6)]
+ p discount2
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
+ p initial_price_per_unit
+ p price_after_promotion_per_unit
  Promotion.create(shop_id: shop_id,
  title: title,
  product_type: product_type,
  initial_quantity: [10, 20, 30, 40, 50, 60].sample,
  remaining_quantity: (initial_quantity - (0...initial_quantity).to_a.sample),
- unit: "piece",
+ unit: "Piece(s)",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
- promotion_status: false,
+ validity: Time.now + (3600..36000).to_a.sample,
+ promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
  min_quantity: 1,
@@ -245,116 +243,64 @@ christmas_fpic = ["food/christmas/cupcake.jpg","food/christmas/cookies.jpg"]
 
  shop_id = Shop.all.last.id
  shop = Shop.find(shop_id)
- product_type = "Fruit"
- for index in 0 ... fruits_t.size
-    title = fruits_f[index]
-    photo_url = fruits_fpic[index].to_s
+ product_type = "Soup"
+ for index in 0 ... sushi_t.size
+    title = soup_t[index]
+    photo_url = soup_tpic[index].to_s
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (3..6).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+ p discount
  discount2 = 1 - discount[rand(0..6)]
+ p discount2
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
+ p initial_price_per_unit
+ p price_after_promotion_per_unit
  Promotion.create(shop_id: shop_id,
  title: title,
  product_type: product_type,
  initial_quantity: [10, 20, 30, 40, 50, 60].sample,
  remaining_quantity: (initial_quantity - (0...initial_quantity).to_a.sample),
- unit: "kg",
+ unit: "Piece(s)",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
- promotion_status: false,
+ validity: Time.now + (3600..36000).to_a.sample,
+ promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
  min_quantity: 1,
  price_after_promotion_per_unit:price_after_promotion_per_unit)
  end
 
-
- shop_id = Shop.all.last.id
+  shop_id = Shop.all.last.id
  shop = Shop.find(shop_id)
- product_type = "Vegetable"
- for index in 0 ... vegetables_f.size
-    title = vegetables_f[index]
-    photo_url = vegetables_fpic[index].to_s
+ product_type = "Pasta"
+ for index in 0 ... sushi_t.size
+    title = pasta_t[index]
+    photo_url = pasta_tpic[index].to_s
     p product_type
     p title
  initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
+ initial_price_per_unit = (8..12).to_a.sample
  discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+ p discount
  discount2 = 1 - discount[rand(0..6)]
+ p discount2
  price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
+ p initial_price_per_unit
+ p price_after_promotion_per_unit
  Promotion.create(shop_id: shop_id,
  title: title,
  product_type: product_type,
  initial_quantity: [10, 20, 30, 40, 50, 60].sample,
  remaining_quantity: (initial_quantity - (0...initial_quantity).to_a.sample),
- unit: "kg",
+ unit: "Piece(s)",
  photo_url: photo_url,
  description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
- promotion_status: false,
- digits_code: 2432,
- initial_price_per_unit: initial_price_per_unit,
- min_quantity: 1,
- price_after_promotion_per_unit:price_after_promotion_per_unit)
- end
-
- shop_id = Shop.all.last.id
- shop = Shop.find(shop_id)
- product_type = "Bread"
- for index in 0 ... breads_t.size
-    title = breads_f[index]
-    photo_url = breads_fpic[index].to_s
-    p product_type
-    p title
- initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
- discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
- discount2 = 1 - discount[rand(0..6)]
- price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
- Promotion.create(shop_id: shop_id,
- title: title,
- product_type: product_type,
- initial_quantity: [10, 20, 30, 40, 50, 60].sample,
- remaining_quantity: (initial_quantity - (0...initial_quantity).to_a.sample),
- unit: "piece",
- photo_url: photo_url,
- description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
- promotion_status: false,
- digits_code: 2432,
- initial_price_per_unit: initial_price_per_unit,
- min_quantity: 1,
- price_after_promotion_per_unit:price_after_promotion_per_unit)
- end
-
-
- shop_id = Shop.all.last.id - 1
- shop = Shop.find(shop_id)
- product_type = "Christmas"
- for index in 0 ... christmas_t.size
-    title = christmas_f[index]
-    photo_url = christmas_fpic[index].to_s
-    p product_type
-    p title
- initial_quantity = (1..15).to_a.sample
- initial_price_per_unit = (5..12).to_a.sample
- discount = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
- discount2 = 1 - discount[rand(0..6)]
- price_after_promotion_per_unit = (initial_price_per_unit*(1- discount2)).to_f
- Promotion.create(shop_id: shop_id,
- title: title,
- product_type: product_type,
- initial_quantity: [10, 20, 30, 40, 50, 60].sample,
- remaining_quantity: (initial_quantity - (0...initial_quantity).to_a.sample),
- unit: "piece",
- photo_url: photo_url,
- description: "#{product_type} - #{title}",
- validity: Time.now + (3600..260000).to_a.sample,
- promotion_status: false,
+ validity: Time.now + (3600..36000).to_a.sample,
+ promotion_status: true,
  digits_code: 2432,
  initial_price_per_unit: initial_price_per_unit,
  min_quantity: 1,
