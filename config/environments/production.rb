5ed17192bc476f74620ed935bdb44f0e1f582ae1
Martin Bragard <bragard.martin@gmail.com>
2016-12-21 13:55:45 +0100

    changed initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "gaspiranha.herokuapp.com" }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
@@ -38,7 +39,7 @@ Rails.application.configure do
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
 }
-  Rails.application.routes.default_url_options[:host] = 'gaspiranha.herokuapp.com.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'gaspiranha.herokuapp.com'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
