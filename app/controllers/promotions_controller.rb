76d293edc5bf66878186c16ce0655176a38f8076
Martin Bragard <bragard.martin@gmail.com>
2016-12-23 13:53:49 +0100

    almost done

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -10,7 +10,7 @@ class PromotionsController < ApplicationController
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = t('flash.authorization')
     redirect_to(root_path)
   end
 
